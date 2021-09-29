744751355d54135092c45eb6e170c5c2efe1d315
HETU PATEL <hetupatel.patidar@gmail.com>
2021-09-29 12:19:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# shopping cart
+
+## getting started
+
+install dependencies and start local dev server
+
+```sh
+npm install
+npm start
+```
---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+   {
+      id: 1,
+      name: 'Puma',
+      price: 4999,
+      currency: 'INR',
+      image: 'images/01.jpeg',
+   },
+   {
+      id: 2,
+      name: 'Reebok',
+      price: 2499,
+      currency: 'INR',
+      image: 'images/02.jpeg',
+   },
+   {
+      id: 4,
+      name: 'Adidas',
+      price: 2399,
+      currency: 'INR',
+      image: 'images/03.jpeg',
+   },
+   {
+      id: 6,
+      name: 'Red Chief',
+      price: 1199,
+      currency: 'INR',
+      image: 'images/04.jpeg',
+   },
+   {
+      id: 5,
+      name: 'Sessel',
+      price: 5999,
+      currency: 'INR',
+      image: 'images/05.jpeg',
+   },
+   {
+      id: 3,
+      name: 'Anna',
+      price: 1499,
+      currency: 'INR',
+      image: 'images/06.jpeg',
+   },
+];
+
+export default data;
